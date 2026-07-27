ba3acc8a49426b278a32d9b66c8409716f501f60
Nikita Kumbhar <nkumbhar752@gmail.com>
2026-07-27 19:59:44 +0530

    Added calculator  and swap number programs

---
+++ nikita/swap_number.py
@@ -0,0 +1,12 @@
+# Swap Two Numbers
+print("=====Swap Two Numbers=====")
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+print("\nBefore Swapping")
+print("First Number:",num1)
+print("Second Number:",num2)
+# Swapping
+num1,num2=num2,num1
+print("\nAfter Swapping")
+print("First Number:",num1)
+print("Second Number:",num2)
