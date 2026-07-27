ce9cb443a1c9117a51b4e56c246bd4028bda009c
Kedar Vijay Marathe <149170262+kedar1330@users.noreply.github.com>
2026-07-27 17:55:16 +0530

    Removed all files for creating correct structure

--- fseries.py
+++
@@ -1,8 +0,0 @@
-#take input from user
-n=int(input("Enter the number of terms:"))
-a,b=0,1
-print (f"Fibonacci Series of {n} numbers:")
-#for loop to iterate through the range
-for i in range(n):
-    print(a,end=" ")
-    a,b=b,a+b
